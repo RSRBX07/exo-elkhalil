e19e3733bbdc462792b0e02db68ac35a194add44
elkhalil <save4@gmx.com>
2017-01-09 16:08:55 +0100

    time

--- numbers.rb
+++ numbers.rb
@@ -8,5 +8,15 @@ puts "l année en seconde #{n}"
 puts " mon age en seconde est #{age}"   
 puts Time.now #elle donne la datte actuelle
 puts Time.new ("19/03/1977") # elle donne mon age en seconde
-now = Time.now
-birth_time=Time.new ("0")
+#now = Time.now
+#birth_time=Time.new ("0")
+#origine du Time
+
+annee =Time.now.to_i/n
+puts Time.now.to_i
+puts Time.now
+puts annee
+anneorigine=Time.now.year - annee
+puts Time.now.year
+puts Time.now.to_i%n
+puts anneorigine
