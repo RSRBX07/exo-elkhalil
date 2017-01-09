27a665a7cf91ba1d8299f45717bde89a78b2a5ee
elkhalil <save4@gmx.com>
2017-01-09 16:51:16 +0100

    exercices

--- dice.rb
+++ dice.rb
@@ -1,3 +1,9 @@
 # un programme qui affiche le resultat d'un tirage d un dé de 6 faces'
+
+#roll = 1 + rand 6
+#puts roll
+
 faces=[1,2,3,4,5,6]
-puts rand (1,2,3,4,5,6)
+#roll = faces.shuffle
+roll=faces [ rand 6]
+puts roll
