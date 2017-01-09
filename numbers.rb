9623937c11e6ec54bc6d003dc183a8403c530e6c
elkhalil <save4@gmx.com>
2017-01-09 15:06:25 +0100

    messagedecommit

--- numbers.rb
+++ numbers.rb
@@ -2,6 +2,11 @@
 #exo sur les nombres
 # 1 calculez le nombre de seconde d une année
 n = (365 * 24) * 3600
+
 age= n*40
 puts "l année en seconde #{n}"
-puts " mon age en seconde est #{age}"
+puts " mon age en seconde est #{age}"   
+puts Time.now #elle donne la datte actuelle
+puts Time.new ("19/03/1977") # elle donne mon age en seconde
+now = Time.now
+birth_time=Time.new ("0")
