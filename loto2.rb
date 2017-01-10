534d14df532c9a35795ffd80a9feecec9a676a81
elkhalil <save4@gmx.com>
2017-01-10 15:06:23 +0100

    ajouter

--- loto2.rb
+++ loto2.rb
@@ -2,9 +2,11 @@
 #entre 100..500 euros
 #le vendredi 13, la cagnote est 2  millions
 is_vendredi_13 = true
-if is_vendredi_13 
-    cagnote=2_000_000
-    else
-      cagnote=100.000
 cagnote = 100.000
+if is_vendredi_13 
+    cagnote = 2.000.000
+    else
+      cagnote = 100.000
+      end
+
 puts " le montant de la gagnote du jour est de #{cagnote}"
