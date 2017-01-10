f2c3eca42695f3cbd5f4416201c87dfcac8273c6
elkhalil <save4@gmx.com>
2017-01-10 14:58:24 +0100

    exercices

---
+++ loto2.rb
@@ -0,0 +1,10 @@
+#affichage dela cagnote
+#entre 100..500 euros
+#le vendredi 13, la cagnote est 2  millions
+is_vendredi_13 = true
+if is_vendredi_13 
+    cagnote=2_000_000
+    else
+      cagnote=100.000
+cagnote = 100.000
+puts " le montant de la gagnote du jour est de #{cagnote}"
