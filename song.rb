795d85cdb94a2c4abb6d3e33805f9ed5271fecc7
elkhalil <save4@gmx.com>
2017-01-10 16:30:27 +0100

    essayer

---
+++ song.rb
@@ -0,0 +1,25 @@
+=begin
+Ma poule n'a plus qu'vingt-neuf poussins
+Ma poule n'a plus qu'vingt-neuf poussins
+Elle en avait trente
+Allongeons la jambe !
+
+Refrain
+Allongeons la jambe, la jambe
+Car la route est longue !
+Allongeons la jambe, la jambe
+Car la route est longue !
+
+Ma poule n'a plus qu'vingt-huit poussins
+Ma poule n'a plus qu'vingt-huit poussins
+Elle en avait trente
+Allongeons la jambe !
+
+Refrain
+=end
+#[0, 1, 2, 3, 4, 5].each do |i|
+  
+  puts "Ma poule n'a plus qu'vingt-neuf poussins"
+ puts "Ma poule n'a plus qu'vingt-neuf poussins"
+ puts "Elle en avait trente"
+ puts "Allongeons la jambe !"
