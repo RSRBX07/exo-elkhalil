a0496316883e5e0d36885f558e124c7d4c53521c
elkhalil <save4@gmx.com>
2017-01-10 15:07:40 +0100

    tableau

--- dice.rb
+++ dice.rb
@@ -1,9 +1,13 @@
 # un programme qui affiche le resultat d'un tirage d un dé de 6 faces'
+#tableau = (1..45).to_a
+#result=[]
 
-#roll = 1 + rand 6
-#puts roll
-
+5.times {
+    var1= rand(tableau.size)
+    result.push(tableau[var1])
+    tableau.delete_at(var1)
+}
 faces=[1,2,3,4,5,6]
 #roll = faces.shuffle
-roll=faces [ rand 6]
-puts roll
+#roll=faces [ rand 6]
+#puts roll
