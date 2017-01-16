7cafdcdaf9eaa570d524f5390924bf97399843b1
elkhalil <save4@gmx.com>
2017-01-16 11:02:50 +0100

    oui

---
+++ test1.rb
@@ -0,0 +1,26 @@
+
+#test de dice.rb
+# require './dice.rb'
+
+# my_dice = Dice.new
+
+# puts "lance de de normal "
+# puts my_dice.roll
+# puts " lance de de pipe"
+# puts my_dice.roll 5
+
+
+
+#test de loto
+require './loto.rb'
+my_grille = Grille.new
+my_tirage = Tirage.new
+
+puts " la grille est "
+puts my_gille.demande_de_grille
+puts my_gille.input_refinement raw_grid
+
+
+puts " mon tirage est "
+puts my_tirage.make_draw
+puts my_tirage.show_result sorted_grid, sorted_draw
