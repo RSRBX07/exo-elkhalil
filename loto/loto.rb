ab466c32ec71a4f6ef5473312283968f847938c9
elkhalil <save4@gmx.com>
2017-01-17 14:56:53 +0100

    oui

--- loto/loto.rb
+++ loto/loto.rb
@@ -74,8 +74,8 @@ def initialize # cette methode va initialiser la variable d instance @picked_bal
     available_balls = (1..45).to_a
     # shuffle balls and take 5
     # @picked_balls ||= available_balls.shuffle.take(5)
-    @picked_balls = @picked_balls || (available_balls.shuffle.take 5)
-
+    #@picked_balls = @picked_balls || (available_balls.shuffle.take 5)
+ @picked_balls = (available_balls.shuffle.take 5)
     puts "Le tirage du jour est : #{@picked_balls.sort}" 
     @picked_balls
   end
