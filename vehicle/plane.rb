b2272f064f2f145698029c898d992475e3b11da9
elkhalil <save4@gmx.com>
2017-01-18 16:40:18 +0100

    add vehicule

---
+++ vehicle/plane.rb
@@ -0,0 +1,12 @@
+class Plane 
+  attr_reader : position
+def initializ 
+  @position = "Roubaix"
+ end 
+def atterir
+  puts " l'avion atterit "
+ end
+ def decoller 
+  puts " l'avion decolle "
+ end
+end
