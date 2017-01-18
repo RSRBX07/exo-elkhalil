d6d8f64120ab68534d005d23e8d70fe486c11a36
elkhalil <save4@gmx.com>
2017-01-18 09:04:58 +0100

    add rspec

--- spec/loto_spec.rb
+++ spec/loto_spec.rb
@@ -40,7 +40,7 @@
 
 
 
- require './loto.rb'
+ require './loto/loto.rb'
  
  
 #    my_loto = Loto.new
@@ -65,25 +65,25 @@
 # test de loto
 
   
-require './loto.rb'
+#require './loto.rb'
 #require './loto/loto.rb'
 # require './french_numbers.rb'
 # fixnumber = Fixnumber.new
 # fixnumber.french_numbers 2
 # fixnumber.num_to_string 10
 
-loto_du_jour = Loto.new
+# loto_du_jour = Loto.new
 
-puts "Avant le tirage, le tirage est #{loto_du_jour.picked_balls.inspect}"
-ma_grille =  Loto.get_player_grid
-loto_du_jour.validate_grid ma_grille
-p loto_du_jour.has_winner?
-# loto_du_jour.picked_balls = [1,2,3,4,5] # grace au : attr_writer
+# puts "Avant le tirage, le tirage est #{loto_du_jour.picked_balls.inspect}"
+# ma_grille =  Loto.get_player_grid
+# loto_du_jour.validate_grid ma_grille
 # p loto_du_jour.has_winner?
-# loto_du_jour.validate_grid  [1,2,3,4,5]
-p loto_du_jour.has_winner?
-puts " le tirage est #{loto_du_jour.picked_balls.inspect}"
-#loto_du_jour.prize
+# # loto_du_jour.picked_balls = [1,2,3,4,5] # grace au : attr_writer
+# # p loto_du_jour.has_winner?
+# # loto_du_jour.validate_grid  [1,2,3,4,5]
+# p loto_du_jour.has_winner?
+# puts " le tirage est #{loto_du_jour.picked_balls.inspect}"
+# #loto_du_jour.prize
 
 
 
