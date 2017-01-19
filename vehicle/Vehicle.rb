feca3e1cde919dc7433d15fb2bc3767905bb6a19
elkhalil <save4@gmx.com>
2017-01-19 11:41:18 +0100

    add vehicle

--- vehicle/Vehicle.rb
+++ vehicle/Vehicle.rb
@@ -4,6 +4,7 @@ attr_reader : position
 def initializ 
   @position = "Roubaix"
  end 
+
 def move 
   puts " I moving "
  end
--- vehicle/plane.rb
+++ vehicle/plane.rb
@@ -1,11 +1,14 @@
 class Plane 
   attr_reader : position
+
 def initializ 
   @position = "Roubaix"
  end 
+
 def atterir
   puts " l'avion atterit "
  end
+ 
  def decoller 
   puts " l'avion decolle "
  end
