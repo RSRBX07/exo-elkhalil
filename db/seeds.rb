48d1e986b07e3d7f5abd9b8624358b2f992a9b1d
elkhalil <save4@gmx.com>
2017-01-24 11:54:40 +0100

    show loto names

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Game.destroy_all
-Game.create[
+Game.create [
   {name: "loto du jour", description: "le tirage est 12,42,13 et 18", players: 245879},
-  {name: "loto du jour", description: "le tirage est   12,2,4,et 56 ",players: 245879},
-  {name: "loto du jour", description: "le tirage est 45,565,23 et 78",players: 245879},
-  {name: "loto du jour", description: "le tirage est12,5,23 et 55 ",players: 245879},
-  {name: "loto du jour", description: "le tirage est 12,15,26 et 89",players: 245879}
+  {name: "loto du jour", description: "le tirage est   12,2,4,et 56 ", players: 245879},
+  {name: "loto du jour", description: "le tirage est 45,565,23 et 78", players: 245879},
+  {name: "loto du jour", description: "le tirage est12,5,23 et 55 ", players: 245879},
+  {name: "loto du jour", description: "le tirage est 12,15,26 et 89", players: 245879}
 ]
